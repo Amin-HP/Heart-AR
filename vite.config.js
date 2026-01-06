268ff5f4506c710de3f15560b74e4d6c14fe5410
Amin-HP <ma.haghpanah@gmail.com>
2026-01-06 14:49:58 +0000

    deploy url error

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,12 @@ export default defineConfig({
     base: '/Heart-AR/',
     build: {
         outDir: 'dist',
+        rollupOptions: {
+            external: [
+                'three',
+                'three/addons/renderers/CSS3DRenderer.js',
+                'mind-ar/dist/mindar-image-three.prod.js'
+            ]
+        }
     },
 });
