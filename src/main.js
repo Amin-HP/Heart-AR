524a93e3a99cc8f02ee25bd832b06f3cc4cd0799
Amin-HP <ma.haghpanah@gmail.com>
2026-01-06 14:09:53 +0000

    first commit

---
+++ src/main.js
@@ -0,0 +1,27 @@
+import './style.css';
+import { MindARThree } from 'mind-ar/dist/mindar-image-three.prod.js';
+import * as THREE from 'three';
+
+document.querySelector('#app').innerHTML = `
+  <div id="container"></div>
+`;
+
+const mindarThree = new MindARThree({
+  container: document.querySelector('#container'),
+  imageTargetSrc: '/assets/targets.mind',
+});
+const {renderer, scene, camera} = mindarThree;
+
+const anchor = mindarThree.addAnchor(0);
+const geometry = new THREE.BoxGeometry(1, 1, 1);
+const material = new THREE.MeshNormalMaterial();
+const cube = new THREE.Mesh( geometry, material );
+anchor.group.add(cube);
+
+const start = async() => {
+  await mindarThree.start();
+  renderer.setAnimationLoop(() => {
+    renderer.render(scene, camera);
+  });
+}
+start();
