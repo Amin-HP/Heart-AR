2dbd86531aaf144c7dc414346e7daa86678efa3f
Amin-HP <ma.haghpanah@gmail.com>
2026-01-06 15:21:40 +0000

    .

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,7 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-    base: '/Heart-AR/',
     build: {
         outDir: 'dist',
-        rollupOptions: {
-            external: [
-                'three',
-                'three/addons/renderers/CSS3DRenderer.js',
-                'mind-ar/dist/mindar-image-three.prod.js'
-            ]
-        }
     },
 });
